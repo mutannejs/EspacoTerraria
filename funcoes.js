56b828bd9eabffd6ad465001214bf5695d882eea
mutannejs <murillojustinoklein@gmail.com>
2023-09-23 19:09:48 -0300

    first commit

---
+++ funcoes.js
@@ -0,0 +1,35 @@
+function clickMenu() {
+	if (m_burguer.style.display == 'block')
+		m_burguer.style.display = 'none';
+	else
+		m_burguer.style.display = 'block';
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
